6884153fb0a29b9ade827f0a8dbb0955205a1843
Praveen Kulkarni <praveenkulkarni1996@gmail@com>
2026-02-21 21:24:56 +0000

    Add llvm installation.

---
+++ dotfiles/llvm.zsh
@@ -0,0 +1,6 @@
+# Add build-tools to your PATH
+export PATH="/home/linuxbrew/.linuxbrew/opt/binutils/bin:$PATH"
+export PATH="/home/linuxbrew/.linuxbrew/opt/llvm/bin:$PATH"
+export PATH="/home/linuxbrew/.linuxbrew/opt/make/bin:$PATH"
+
+export LD_LIBRARY_PATH="/home/linuxbrew/.linuxbrew/lib:$LD_LIBRARY_PATH"
